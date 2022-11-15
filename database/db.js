64868d24b5b398187b4351eafc8693144adf5ff5
hugogoncalvez <hugogoncalvez@gmail.com>
2022-11-15 10:30:57 -0300

    se agrega el dialect

--- database/db.js
+++ database/db.js
@@ -3,7 +3,8 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from '../config.js'
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    port: DB_PORT
+    port: DB_PORT,
+    dialect: 'mysql'
 })
 
 export default db;
